c0827be84109137a279f9ecee5125454f0462a16
Martín <martinsps96@gmail.com>
2020-05-14 10:04:27 +0200

    Non validating select field addition

--- application/forms.py
+++ application/forms.py
@@ -3,20 +3,30 @@ from wtforms import StringField, SubmitField, DecimalField, FileField, IntegerFi
 from wtforms.validators import DataRequired, NumberRange, Optional
 
 
+class NonValidatingSelectField(SelectField):
+    """
+    Attempt to make an open ended select multiple field that can accept dynamic
+    choices added by the browser.
+    """
+
+    def pre_validate(self, form):
+        pass
+
+
 class CN2SDForm(FlaskForm):
     input_file = FileField('Input data (.csv file)', validators=[DataRequired()])
-    output_column = StringField('Output column name', validators=[DataRequired()])
-    positive_class = StringField('Positive class value', validators=[DataRequired()])
-    max_expressions = IntegerField('Maximum expressions in search',
-                                   validators=[DataRequired(message="Must be an integer >= 1."),
+    output_column = NonValidatingSelectField('Output column name', choices=[], validators=[])
+    positive_class = NonValidatingSelectField('Positive class value', choices=[], validators=[])
+    max_expressions = IntegerField('Maximum expressions in search (integer >= 1, default 3)',
+                                   validators=[Optional(),
                                                NumberRange(min=1, message="Must be an integer >= 1")])
-    min_wracc = DecimalField('Minimum WRAcc',
-                             validators=[DataRequired(message="Must be a decimal between 0 and 1."),
+    min_wracc = DecimalField('Minimum WRAcc (decimal in (0,1], default 0.05)',
+                             validators=[Optional(),
                                          NumberRange(min=0.000001, max=1,
                                                      message="Must be a decimal > 0 and <= 1.")])
     methods = [(0, "No weight method (normal CN2)"), (1, "Multiplicative"), (2, "Additive")]
     weight_method = SelectField('Weight method', choices=methods, coerce=int, validators=[])
-    gamma = DecimalField('Gamma parameter for multiplicative weight method (default 0.5)',
+    gamma = DecimalField('Gamma parameter for multiplicative weight method (decimal in (0,1), default 0.5)',
                          validators=[Optional(), NumberRange(min=0.000001, max=0.999999,
                                                              message="Must be a decimal > 0 and < 1.")])
     submit = SubmitField('Start!')
@@ -24,21 +34,21 @@ class CN2SDForm(FlaskForm):
 
 class PRIMForm(FlaskForm):
     input_file = FileField('Input data (.csv file)', validators=[DataRequired()])
-    output_column = StringField('Output column name', validators=[DataRequired()])
-    positive_class = StringField('Positive class value', validators=[DataRequired()])
-    alpha = DecimalField('Alpha',
-                         validators=[DataRequired(message="Must be a decimal between 0 and 1."),
+    output_column = NonValidatingSelectField('Output column name', choices=[], validators=[])
+    positive_class = NonValidatingSelectField('Positive class value', choices=[], validators=[])
+    alpha = DecimalField('Alpha (decimal in (0,1), default 0.1)',
+                         validators=[Optional(),
                                      NumberRange(min=0.000001, max=0.999999,
                                                  message="Must be a decimal > 0 and < 1.")])
-    threshold_box = DecimalField('Box support threshold',
-                                 validators=[DataRequired(message="Must be a decimal between 0 and 1."),
+    threshold_box = DecimalField('Box support threshold (decimal in (0,1), default 0.1)',
+                                 validators=[Optional(),
                                              NumberRange(min=0.000001, max=0.999999,
                                                          message="Must be a decimal > 0 and < 1.")])
-    threshold_global = DecimalField('Global support threshold',
-                                    validators=[DataRequired(message="Must be a decimal between 0 and 1."),
+    threshold_global = DecimalField('Global support threshold (decimal in (0,1), default 0.1)',
+                                    validators=[Optional(),
                                                 NumberRange(min=0.000001, max=0.999999,
                                                             message="Must be a decimal > 0 and < 1.")])
-    min_mean = DecimalField('Minimum mean (default is global mean)',
+    min_mean = DecimalField('Minimum mean (decimal in (0,1), default is global mean)',
                             validators=[Optional(),
                                         NumberRange(min=0.000001, max=0.999999,
                                                     message="Must be a decimal > 0 and < 1.")])
